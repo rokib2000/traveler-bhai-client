2c9f88705e5dcc244696b9628f974864b3943055
rokib2000 <rokib0w1@gmail.com>
2022-11-14 00:11:39 +0600

    added signup page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -20,24 +20,59 @@ const Login = () => {
             <h1 className="text-5xl font-bold mt-[-0.5rem]">Login now!</h1>
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body">
+            <form className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" placeholder="email" className="input input-bordered" />
+                <input type="email" placeholder="email" name="email" className="input input-bordered" required />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="password" placeholder="password" className="input input-bordered" />
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                  required
+                />
                 <label className="label">
                   <Link className="label-text-alt link link-hover">Forgot password? </Link>
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <button className="btn btn-primary">Log in</button>
+              </div>
+            </form>
+            <p className="text-center ">
+              New to Traveler Bhai{" "}
+              <Link className="text-cyan-500 font-semibold" to="/signup">
+                Sign Up
+              </Link>{" "}
+            </p>
+            <div className="divider">OR</div>
+            <div className="card-body">
+              <div className="form-control ">
+                <button type="button" class="btn btn-primary">
+                  <svg
+                    class="mr-2 -ml-1 w-4 h-4"
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fab"
+                    data-icon="google"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 488 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                    ></path>
+                  </svg>
+                  Log in with Google
+                </button>
               </div>
             </div>
           </div>
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,9 +1,91 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>
-      <h1>sign up</h1>
+    <div
+      className="min-h-screen"
+      style={{
+        backgroundImage: `url("https://i.postimg.cc/B6rNsh9j/logo-maiya.png") `,
+        backgroundRepeat: "no-repeat",
+        // backgroundSize: "40%",
+        // backgroundAttachment: "fixed",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col ">
+          <div className="text-center ">
+            <h1 className="text-5xl font-bold mt-[-0.5rem]">Sign Up now!</h1>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" placeholder="name" name="name" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Image</span>
+                </label>
+                <input type="text" placeholder="image url" name="image" className="input input-bordered" />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Sign up</button>
+              </div>
+            </form>
+            <p className="text-center mb-6">
+              Already have an account?{" "}
+              <Link className="text-cyan-500 font-semibold" to="/login">
+                Log in
+              </Link>{" "}
+            </p>
+            {/* <div className="divider">OR</div> */}
+            {/* <div className="card-body">
+              <div className="form-control ">
+                <button type="button" class="btn btn-primary">
+                  <svg
+                    class="mr-2 -ml-1 w-4 h-4"
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fab"
+                    data-icon="google"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 488 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                    ></path>
+                  </svg>
+                  Log in with Google
+                </button>
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
