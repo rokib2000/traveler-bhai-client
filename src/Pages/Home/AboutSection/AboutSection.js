026ac460c32c30fbe0679992fe6dbc728384e20b
rokib2000 <rokib0w1@gmail.com>
2022-11-13 16:33:10 +0600

    home page about section added

---
+++ src/Pages/Home/AboutSection/AboutSection.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AboutSection = () => {
+  return (
+    <div className="hero  ">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src="https://i.postimg.cc/150xwgvD/Woman-With-Suitcase.png" className="max-w-sm rounded-lg " />
+        <div>
+          <h1 className="text-5xl font-bold">TRAVEL MORE TO DISCOVER YOURSELF!</h1>
+          <p className="py-6">
+            Air seed winged lights saw kind whales in sixth dont seas dron image so fish all tree meat dont there is
+            seed winged lights saw kind whales in sixth dont seas dron image so fish all tree meat dont there
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import AboutSection from "../AboutSection/AboutSection";
 import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div className="container mx-auto">
       <Banner />
+      <AboutSection />
     </div>
   );
 };
