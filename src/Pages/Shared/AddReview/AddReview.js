c3649ef417c4a827abf51402e9213c0b472bba7d
rokib2000 <rokib0w1@gmail.com>
2022-11-15 05:27:49 +0600

    add review fix

--- src/Pages/Shared/AddReview/AddReview.js
+++ src/Pages/Shared/AddReview/AddReview.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../../contexts/AuthProvider";
 
 const AddReview = ({ service }) => {
   const { user } = useContext(AuthContext);
   //   console.log(user);
-  const { displayName, email, photoURL } = user;
+  //   const { displayName, email, photoURL } = user;
 
   const { _id } = service;
 
@@ -15,7 +16,13 @@ const AddReview = ({ service }) => {
     const form = event.target;
     const body = form.body.value;
 
-    const review = { review: body, userName: displayName, userEmail: email, userPhoto: photoURL, serviceID: _id };
+    const review = {
+      review: body,
+      userName: user?.displayName,
+      userEmail: user?.email,
+      userPhoto: user?.photoURL,
+      serviceID: _id,
+    };
 
     fetch("http://localhost:5000/reviews", {
       method: "POST",
@@ -37,7 +44,7 @@ const AddReview = ({ service }) => {
     <div className="md:w-1/2 mx-auto">
       <form onSubmit={handleReviewSubmit} className="w-full  bg-white rounded-lg px-4 pt-2">
         <div className="flex flex-wrap -mx-3 mb-6">
-          <h2 className="px-4 pt-3 pb-2 text-gray-800 text-lg">Add a new review</h2>
+          <h2 className="px-4 pt-3 pb-2 text-gray-800 text-xl font-medium">Add a new review</h2>
           <div className="w-full md:w-full px-3 mb-2 mt-2">
             <textarea
               className="textarea textarea-bordered w-full"
@@ -46,9 +53,37 @@ const AddReview = ({ service }) => {
               required
             ></textarea>
           </div>
-          <div className="w-full md:w-full flex items-start  px-3">
+          <div className="w-full md:w-full flex items-start  px-3 mb-8">
             <div className="-mr-1">
-              <input type="submit" className="btn btn-primary" value="Post Review" />
+              {user?.uid ? (
+                <button type="submit" className="btn btn-primary">
+                  Post Review
+                </button>
+              ) : (
+                <div className="alert  alert-error shadow-lg">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      className="stroke-info flex-shrink-0 w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      ></path>
+                    </svg>
+                    <span>Please login to write a review</span>
+                  </div>
+                  <div className="flex-none">
+                    <Link to="/login" className="btn btn-sm btn-primary">
+                      Log In
+                    </Link>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
